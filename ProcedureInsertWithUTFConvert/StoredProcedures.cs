ce2e246eb55f58e042c773c46f95d807eafddea3
FabricioK <fabriciodossa@hotmail.com>
2016-04-15 18:01:56 -0300

    alterando

--- ProcedureInsertWithUTFConvert/StoredProcedures.cs
+++ ProcedureInsertWithUTFConvert/StoredProcedures.cs
@@ -4,6 +4,8 @@ using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
 using System.Text;
+using System.IO;
+using System.Collections.Generic;
 
 public partial class StoredProcedures
 {
@@ -18,39 +20,77 @@ public partial class StoredProcedures
 
         return myString;
     }
-
+    public class ListaColunas {
+         public List<string> values = new List<string>();
+    }
     [SqlProcedure()]
     public static void ProcedureInsertWithUTFConvert(
-        SqlString table, SqlString name)
+        SqlString de, SqlString para)
     {
-       
+        List<ListaColunas> values = new List<ListaColunas>();
+        List<string> columns = new List<string>();
+        List<Type> tipos = new List<Type>();
         using (SqlConnection conn = new SqlConnection("context connection=true"))
         {
             conn.Open();
             using (SqlCommand selectFields = new SqlCommand(
-             "SELECT " +
-             " * " +
-             " FROM @tabela"
+             "SELECT  * FROM " + de.ToString()
              ,
              conn))
             {
-                SqlParameter tabelaParam = selectFields.Parameters.Add(
-                    "@tabela",
-                    SqlDbType.VarChar);
-                tabelaParam.Value = table;
-
+                DataSet selectResults = new DataSet();
                 using (SqlDataReader fieldsReader = selectFields.ExecuteReader())
-                {
+                {                  
+
+                    for (int i = 0; i < fieldsReader.FieldCount; i++)
+                    {
+                        columns.Add(fieldsReader.GetName(i));
+                    }
+
+                    for (int i = 0; i < fieldsReader.FieldCount; i++)
+                    {
+                        tipos.Add(fieldsReader.GetFieldType(i));
+                    }
+
                     while (fieldsReader.Read())
                     {
-                        SqlString emailAddress = fieldsReader.GetSqlString(1);
-                      
-
+                        fieldsReader.GetFieldType(0);
+                        var novosCampos =  new ListaColunas ();
+                        List<string> campos = new List<string>();
+                        for (int i = 0; i < fieldsReader.FieldCount; i++)
+                        {
+                            campos.Add(fieldsReader[i].ToString());
+                        }
+                        novosCampos.values = campos;
                     }
                 }
             }
 
-        }
+            foreach (ListaColunas value in values)
+            {
+                string  commandoInsert = "INSERT "+ para.ToString()+ "(";
+                foreach (var col in columns)
+                    commandoInsert = commandoInsert + col + ",";
 
+                commandoInsert = commandoInsert.TrimEnd(',');
+
+                commandoInsert  = commandoInsert + ") VALUES (";
+                var cnt = 0;
+                foreach (var val in value.values)
+                {
+                    tipos[cnt].
+                    commandoInsert = commandoInsert + ConverteText(val) + ",";
+
+                    cnt =+1;
+                }
+                commandoInsert = commandoInsert + ")";
+
+                using (SqlCommand objInsertCommand = new SqlCommand(commandoInsert, conn))
+                {
+                    objInsertCommand.ExecuteNonQuery();
+                }
+            }
+            conn.Close();
+        }
     }
 }
